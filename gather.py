6279a11f4c229cc6c758fd4d8ca82d655f706ea3
Samuele Poppi <s.poppi94@gmail.com>
2020-11-02 22:12:16 +0100

    edited gather

--- gather.py
+++ gather.py
@@ -16,7 +16,8 @@ l=[]
 #visu=2
 #lst=lst[int(len(lst)/2):] if visu == 2 else lst[:int(len(lst)/2)]
 print(len(lst))
-for d in lst[287-12:]:
+for d in lst:
+  #print(d)
   val=list(d.values())
   cam=list(d.keys())[0][1]
   CAMS[cam]+=1
@@ -27,7 +28,7 @@ for d in lst[287-12:]:
   inst[list(CAMS.keys()).index(cam)] += float(val[0][1][1])
 
 print('\n----------------------------------------------------------------------------------------------------------------------------')
-print('\n\t\t\t| Average Drop\t\t| Increase In Confidence\t| Deletion\t\t| Insertion')
+print('\n\t\t\t|  Average Drop\t\t|  Increase In Confidence\t| Deletion\t\t|  Insertion')
 for c in CAMS:
     key=c
     if len(key)<5:
@@ -36,7 +37,7 @@ for c in CAMS:
         key+='\t'
     print(f'''
     ------------------------------------------------------------------------------------------------------------------------\n
-    {key}\t| {round(avgdrop[list(CAMS.keys()).index(c)]/CAMS[c],2):.2f}%\t\t| {round(incinconf[list(CAMS.keys()).index(c)]/CAMS[c],2):.2f}%\t\t\t| {round(delt[list(CAMS.keys()).index(c)]/CAMS[c],3):.3f}\t\t\t| {round(inst[list(CAMS.keys()).index(c)]/CAMS[c],3):.3f}''')
+    {key}\t|  {round(avgdrop[list(CAMS.keys()).index(c)]/CAMS[c],2):.2f}%\t\t|  {round(incinconf[list(CAMS.keys()).index(c)]/CAMS[c],2):.2f}%\t\t\t| {round(delt[list(CAMS.keys()).index(c)]/CAMS[c],3):.3f}\t\t\t|  {round(inst[list(CAMS.keys()).index(c)]/CAMS[c],3):.3f}''')
 print('\n----------------------------------------------------------------------------------------------------------------------------')
 #for x in l:
 #    print(x[1])
